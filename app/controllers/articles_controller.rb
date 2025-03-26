e80f2bd06488fb63839719019a547dc597ff44df
arruda95 <two.vitor@gmail.com>
2025-03-25 22:08:23 -0300

    Adicionei o create no controller e view/index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     #Busca todos artigos ordenados por data
     @articles = Article.all.order(created_at: :desc)
   end
+
+  def show
+    @article = Article.find(params[:d]) 
+  end
 end
